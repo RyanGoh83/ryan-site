9daa9ec414108927d951fe36418b83c877319a7c
Ryan Goh <gohry2@gmail.com>
2017-07-29 17:13:00 +0800

    about me, header, favicon.ico complete

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -8,6 +8,10 @@ app.config["DEBUG"] = True
 @app.route('/')
 def homepage():
 	return render_template("main.html")
-	
+
+@app.route('/about_me/')
+def about_me():
+        return render_template("about_me.html")
+
 if __name__ == "__main__":
 	app.run()
